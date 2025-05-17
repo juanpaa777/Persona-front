60adc948cb4c743dd5e447f757374b5057a63161
juanpaa777 <156286909+juanpaa777@users.noreply.github.com>
2025-05-16 19:03:16 -0600

    Update persona.service.ts

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Persona } from '../models/persona.model';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class PersonaService {
