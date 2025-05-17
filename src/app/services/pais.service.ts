c6167c9beee8c1f00c4372716aa50c122a8b01b3
juanpaa777 <156286909+juanpaa777@users.noreply.github.com>
2025-05-16 19:03:06 -0600

    Update pais.service.ts

--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pais } from '../models/pais.model';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class PaisService {
