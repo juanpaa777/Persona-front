10b3a9d2003030537e4a1c40a0a08a3068711b17
juanpaa777 <156286909+juanpaa777@users.noreply.github.com>
2025-05-16 18:56:18 -0600

    Update estado.service.ts

--- src/app/services/estado.service.ts
+++ src/app/services/estado.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Estado } from '../models/estado.model';
-import { environment } from '../../environments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class EstadoService {
